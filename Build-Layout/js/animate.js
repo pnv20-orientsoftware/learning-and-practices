59866b778c5ce80311e144a404e36fd30a32cbb2
Tai Tran Van <tai.tran@orientsoftware.com>
2019-06-05 14:27:13 +0700

    WIP - finished navbar collapse

---
+++ Build-Layout/js/animate.js
@@ -0,0 +1,46 @@
+var isClosedMenu = true;
+$(document).ready(function() {
+
+    $("#open-menu").click(function() {
+        isClosedMenu = false;
+        $("body").animate({
+            left: "240px"
+        }, 350);
+        $('.navbar-nav').animate({
+            left: "0px"
+        }, 350);
+        $(this).hide();
+        $("#close-menu").show();
+    });
+
+    $("#close-menu").click(function() {
+        isClosedMenu = true;
+        $("body").animate({
+            left: "0px"
+        }, 350);
+        $('.navbar-nav').animate({
+            left: "-224px"
+        }, 350);
+        $(this).hide();
+        $("#open-menu").show();
+    });
+
+});
+
+$(window).on('resize', function() {
+
+    if ($(this).width() >= 767) {
+
+        if (!isClosedMenu) {
+            $("#close-menu").click();
+        }
+
+        $("#open-menu").hide();
+        $("#close-menu").hide();
+
+    } else {
+        $("#open-menu").show();
+        $("#close-menu").hide();
+    }
+
+});

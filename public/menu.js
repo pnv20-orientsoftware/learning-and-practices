79b3d1227e91f871e035b6fb6091a82c0ba0ef68
vihoang1604 <vi.hoang@student.passerellesnumeriques.org>
2019-06-07 17:03:12 +0700

    Final code

--- public/menu.js
+++ public/menu.js
@@ -1,6 +1,46 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+var isClosedMenu = true;
+$(document).ready(function() {
 
-navBarToggle.addEventListener('click', function() {
-    mainNav.classList.toggle('active');
+    $("#open-menu").click(function() {
+        isClosedMenu = false;
+        $("body").animate({
+            left: "235px"
+        }, 350);
+        $('.navbar-nav').animate({
+            left: "0px"
+        }, 350);
+        $(this).hide();
+        $("#close-menu").show();
+    });
+
+    $("#close-menu").click(function() {
+        isClosedMenu = true;
+        $("body").animate({
+            left: "0px"
+        }, 350);
+        $('.navbar-nav').animate({
+            left: "-245px"
+        }, 350);
+        $(this).hide();
+        $("#open-menu").show();
+    });
+
+});
+
+$(window).on('resize', function() {
+
+    if ($(this).width() >= 767) {
+
+        if (!isClosedMenu) {
+            $("#close-menu").click();
+        }
+
+        $("#open-menu").hide();
+        $("#close-menu").hide();
+
+    } else {
+        $("#open-menu").show();
+        $("#close-menu").hide();
+    }
+
 });
